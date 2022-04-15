cbfb45162f73dbe30e068ae30e7aad9f3de6d79f
Hdat298 <hdat318@gmail.com>
2022-04-15 16:37:28 +0700

    Revert "Final"
    This reverts commit 9685197939ffdd90cdaf164486f8f6e23de52bb7.

--- src/test/java/com/virtualclassroom/UserRepositoryTest.java
+++ src/test/java/com/virtualclassroom/UserRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.virtualclassroom;
 
-import com.virtualclassroom.model.Role;
 import com.virtualclassroom.model.User;
 import com.virtualclassroom.repository.RoleRepository;
 import com.virtualclassroom.repository.UserRepository;
@@ -10,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
-import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -29,24 +27,9 @@ public class UserRepositoryTest {
         user.setUserName("test");
         user.setUserPassword("test");
         user.setUserEmail("test@gmail.com");
-        user.addRole(roleRepository.findByName("TEACHER"));
+        user.addRole(roleRepository.findByName("STUDENT"));
 
         userRepository.save(user);
         assert user.getRoles().size() == 1;
-    }
-
-    @Test
-    public void testAddRolesToExistingUser() {
-        User user = userRepository.findById(1L).get();
-
-        Role roleUser = roleRepository.findByName("TEACHER");
-        user.addRole(roleUser);
-
-        Role roleAdmin = new Role(2L);
-        user.addRole(roleAdmin);
-
-        User savedUser = userRepository.save(user);
-        assertThat(savedUser.getRoles().size()).isEqualTo(2);
-
     }
 }
