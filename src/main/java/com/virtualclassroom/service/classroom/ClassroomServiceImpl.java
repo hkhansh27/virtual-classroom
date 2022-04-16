7fbb81c6d591a42f2b5de9bff87e79e8cd873a20
Hdat298 <hdat318@gmail.com>
2022-04-17 01:29:18 +0700

    bug unsaved user_class

--- src/main/java/com/virtualclassroom/service/classroom/ClassroomServiceImpl.java
+++ src/main/java/com/virtualclassroom/service/classroom/ClassroomServiceImpl.java
@@ -1,4 +1,19 @@
 package com.virtualclassroom.service.classroom;
 
+import com.virtualclassroom.model.Classroom;
+import com.virtualclassroom.repository.ClassRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClassroomServiceImpl implements ClassroomService {
+    private final ClassRepository classRepository;
+
+    public ClassroomServiceImpl(ClassRepository classRepository) {
+        this.classRepository = classRepository;
+    }
+
+    @Override
+    public void createClass(Classroom classroom) {
+        classRepository.save(classroom);
+    }
 }
