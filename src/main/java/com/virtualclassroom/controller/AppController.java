152b580fa5b59f67c607edb8e7e35134c5cb6111
Hdat298 <hdat318@gmail.com>
2022-04-19 16:21:51 +0700

    add function
    - Add default role when register
    - Edit Role

--- src/main/java/com/virtualclassroom/controller/AppController.java
+++ src/main/java/com/virtualclassroom/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.virtualclassroom.controller;
 
 import com.virtualclassroom.model.Classroom;
+import com.virtualclassroom.model.Role;
 import com.virtualclassroom.model.User;
 import com.virtualclassroom.service.user.UserService;
 import org.jetbrains.annotations.NotNull;
@@ -9,7 +10,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -35,10 +39,11 @@ public class AppController {
 
     @PostMapping("/process_register")
     public String processRegister(@NotNull User user) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getUserPassword());
-        user.setUserPassword(encodedPassword);
-        userService.addUser(user);
+        userService.saveUserWithDefaultRole(user);
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        String encodedPassword = passwordEncoder.encode(user.getUserPassword());
+//        user.setUserPassword(encodedPassword);
+//        userService.addUser(user);
         return "login-register";
     }
 
@@ -59,4 +64,20 @@ public class AppController {
         model.addAttribute("loginError", true);
         return "login-register";
     }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        userService.save(user);
+        return "redirect:/user/index";
+    }
+
+    @GetMapping("users/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        User user = userService.get(id);
+        List<Role> listRoles = userService.getRoles();
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+        return "user_form";
+    }
+
 }
