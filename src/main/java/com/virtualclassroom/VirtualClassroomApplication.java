61b7c5bf9d92d55e489e85be9f867f76461a604c
Huỳnh Hữu Khánh <khanh201011@gmail.com>
2022-05-07 09:40:01 +0700

    refactor and update UI (#14)
    refactor and UI

--- src/main/java/com/virtualclassroom/VirtualClassroomApplication.java
+++ src/main/java/com/virtualclassroom/VirtualClassroomApplication.java
@@ -1,11 +1,7 @@
 package com.virtualclassroom;
 
-import com.virtualclassroom.model.User;
-import com.virtualclassroom.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
 @SpringBootApplication
